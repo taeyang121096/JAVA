47d73d04d03620c49e6972bf5452690192954922
taeyang121096 <taeyang121096@naver.com>
2024-06-20 14:20:00 +0900

    String 기초

---
+++ src/skill/algorithm/StringContact.java
@@ -0,0 +1,33 @@
+package skill.algorithm;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class StringContact {
+
+    public static void main(String[] args) {
+        String str = "123";
+        String str2 = "456";
+
+        char[] charArray = str.toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        String concat = str.concat(str2);
+        System.out.println(concat);
+    }
+
+    public String solution1(String my_string, int[] index_list) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int idx : index_list)
+            sb.append(my_string.charAt(idx));
+
+        return sb.toString();
+    }
+
+    public String solution2(String myString, int[] indexList) {
+        return Arrays.stream(indexList)
+                .mapToObj(operand -> String.valueOf(myString.charAt(operand))).collect(Collectors.joining());
+    }
+}
