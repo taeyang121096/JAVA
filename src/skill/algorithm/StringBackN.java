674d97a945c084c518d9d6a68ffa15251850a95d
taeyang121096 <taeyang121096@naver.com>
2024-06-20 14:20:07 +0900

    String 기초

---
+++ src/skill/algorithm/StringBackN.java
@@ -0,0 +1,12 @@
+package skill.algorithm;
+
+public class StringBackN {
+
+    public static void main(String[] args) {
+        String str = "abcdefg";
+        int n = 2;
+
+//        문자열을 n 자리 만큼 뒤에서 자르는 법
+        System.out.println(str.substring(str.length() - n));
+    }
+}
